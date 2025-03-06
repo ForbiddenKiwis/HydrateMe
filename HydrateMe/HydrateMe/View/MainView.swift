9625138709a6ffaf8c13eec5241566634ae57d2a
jordan artzy-mccendie <127543411+jartzy01@users.noreply.github.com>
2025-03-06 09:48:39 -0500

    created views

---
+++ HydrateMe/HydrateMe/View/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  HydrateMe
+//
+//  Created by english on 2025-03-06.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ HydrateMe/HydrateMe/View/Tab.swift
@@ -0,0 +1,31 @@
+//
+//  Tab.swift
+//  HydrateMe
+//
+//  Created by english on 2025-03-06.
+//
+
+import SwiftUI
+
+struct Tab: View {
+    var body: some View {
+        TabView {
+            MainView()
+                .tabItem{
+                    Image("water 2")
+                }
+            StatisticsView()
+                .tabItem{
+                    Image("line-graph 2")
+                }
+            ProfileView()
+                .tabItem{
+                    Image("ellipse 1")
+                }
+        }
+    }
+}
+
+#Preview {
+    Tab()
+}
